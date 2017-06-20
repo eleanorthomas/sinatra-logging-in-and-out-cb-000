49f480546e5133b9047042e3471ad59af09fea87
Eleanor Thomas <eleanor.thomas.res@gmail.com>
2017-06-20 11:12:16 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+  def self.current_user(session)
+    if session.has_key? :user_id
+      User.find(session[:user_id])
+    end
+  end
+
+  def self.is_logged_in?(session)
+    !!self.current_user(session)
+  end
 end
